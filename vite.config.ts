df9cb1270f4a13ed3567578a3c31ae5c9d0d1f3f
Ilya <bkmy43@googlemail.com>
2025-02-18 23:42:09 +0100

    trying to fix mime error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This is correct for a custom domain
+  base: './',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
