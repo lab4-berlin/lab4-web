a389bcaa92e9187f1414653516713aae13df39d8
Ilya <bkmy43@googlemail.com>
2025-02-20 22:13:06 +0100

    fixing redirect on the client side

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { BlogPost } from './pages/BlogPost';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Small delay to ensure DOM is ready
+    setTimeout(() => {
+      if (location.hash === '#blog') {
+        const blogSection = document.getElementById('blog');
+        if (blogSection) {
+          blogSection.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    }, 100);
+  }, [location, location.hash]); // Also watch for hash changes
+
   return (
     <div className="min-h-screen bg-white">
       <Navigation />
