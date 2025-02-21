548ee4c29be89db9218e5ee1504caed1610778d8
Ilya <bkmy43@googlemail.com>
2025-02-21 12:34:37 +0100

    Revert "trying to fix redirect...again...and again"
    This reverts commit b0e7982fbf9b7d1b059ee3bee59d55458eb582cd.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
-import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { BlogPost } from './pages/BlogPost';
-import { Helmet } from 'react-helmet-async';
 
 function App() {
   const location = useLocation();
@@ -23,21 +22,14 @@ function App() {
 
   return (
     <div className="min-h-screen bg-white">
-      <Helmet>
-        <link rel="canonical" href={`https://lab4.berlin${location.pathname}`} />
-      </Helmet>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/data-mesh-hype" element={<BlogPost />} />
-        {/* Redirect /blog to /#blog */}
-        <Route path="/blog" element={<Navigate to="/#blog" replace />} />
-        {/* Catch all other /blog/* paths and redirect to /#blog */}
-        <Route path="/blog/*" element={<Navigate to="/#blog" replace />} />
       </Routes>
       <Footer />
     </div>
   );
 }
 
-export default App
+export default App;
